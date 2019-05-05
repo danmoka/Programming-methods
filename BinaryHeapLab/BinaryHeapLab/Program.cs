5e4e30b3ab0b7c4358d9ce6428baa924ad68b355
Daniil Potemkin <daniilpot@live.com>
2019-05-05 11:35:14 +0300

    Added test exercise for BinaryHeap

--- BinaryHeapLab/BinaryHeapLab/Program.cs
+++ BinaryHeapLab/BinaryHeapLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BinaryHeapLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,61 @@ namespace BinaryHeapLab
     {
         static void Main(string[] args)
         {
+            int n = 10;
+            Random rd = new Random();
+            Queue<double> goodsWeight = new Queue<double>(); // веса товаров
+
+            for (int i = 0; i < n; i++)
+                goodsWeight.Enqueue(Math.Round(rd.NextDouble(), 3));
+
+            Console.Write("Weights on conveyor: ");
+
+            foreach (var oneWeight in goodsWeight)
+                Console.Write(" {0} |", oneWeight);
+
+            BinaryHeap<double> boxes = new BinaryHeap<double>(); // коробки для упаковки
+
+            boxes.Add(goodsWeight.Dequeue()); // + проверка на отсутствие товара
+
+            while(goodsWeight.Count > 0)
+            {
+                var oneWeight = goodsWeight.Dequeue();
+                var buffer = new List<double>(); // буфер для хранения извлеченных максимумов кучи
+
+                while(boxes.Count > 0)
+                {
+                    var oneBox = boxes.Pop();
+
+                    // если вес товара умещается в коробку, то...
+                    if ( oneBox + oneWeight <= 1 )
+                    {
+                        // добавляем коробку с новым весом, вместо извлеченной
+                        boxes.Add(oneBox + oneWeight);
+                        break;
+                    }
+                    // иначе...
+                    else
+                    {
+                        // запоминаем какой вес вытащили
+                        buffer.Add(oneBox);
+                    }
+                }
+
+                // если подходящая коробка не найдена, то...
+                if (boxes.Count == 0)
+                    // добавляем новую
+                    boxes.Add(oneWeight);
+
+                // добавляем все веса, которые запомнили
+                foreach (var el in buffer)
+                    boxes.Add(el);
+            }
+
+            Console.Write("\nPackaging in {0} boxes completed: ", boxes.Count);
+
+            foreach (var box in boxes.Elements())
+                Console.Write(" {0} |", box);
+
         }
     }
 }
